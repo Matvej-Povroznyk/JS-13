875f8e82a30e401125732570ded06aa1c6b0f901
Matvej-Povroznyk <matvejtop12@gmail.com>
2026-04-04 07:18:49 +0300

    INIT

---
+++ index-13.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+ * ==================
+ *   START OF HW-13
+ * ==================
+ *
+ * HERE IS NO MARKUP.
+ *
+ *
+ */
+
+/* TASK 1 & 2 */
+// 1: NOT READY
+// 2 - HERE WILL BE TASK 2
+
+/*
+ * ================
+ *   END OF HW-13
+ * ================
+ */
